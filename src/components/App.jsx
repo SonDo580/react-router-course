cf30ae3f3efb557aabff267369f6bf3905eb7cdc
Son Do <dominhson580@gmail.com>
2023-03-27 21:46:56 +0700

    Add routes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,21 @@
-export default function App () {
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./Home";
+import NavBar from "./NavBar";
+import Players from "./Players";
+import Teams from "./Teams";
+
+export default function App() {
   return (
-    <div>
-      Hash History Basketball League
-    </div>
-  )
+    <BrowserRouter>
+      <div>
+        <NavBar />
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/players" element={<Players />} />
+          <Route path="/teams" element={<Teams />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
 }
