e3cf56ee8f7254db32265bb1f4adee9cef1282c7
Son Do <dominhson580@gmail.com>
2023-03-28 08:32:43 +0700

    Create component Players

---
+++ src/components/Players.jsx
@@ -0,0 +1,57 @@
+import { Link, useLocation, useSearchParams } from "react-router-dom";
+import usePlayerNames from "../hooks/usePlayerNames";
+import { slugify } from "../utils";
+
+function CustomLink({ to, children }) {
+  const location = useLocation();
+  const playerId = location.pathname.split("/")[2];
+  const match = playerId === to;
+
+  const styles = match
+    ? {
+        fontWeight: 900,
+        color: "var(--white)",
+      }
+    : {};
+
+  return (
+    <Link style={{ ...styles }} to={to}>
+      {children}
+    </Link>
+  );
+}
+
+function SideBar({ title, list }) {
+  return (
+    <div>
+      <h3 className="header">{title}</h3>
+      <ul className="sidebar-list">
+        {list.map((item) => (
+          <li key={item}>
+            <CustomLink to={`${slugify(item)}`}>
+              {item.toUpperCase()}
+            </CustomLink>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default function Players() {
+  const [searchParams] = useSearchParams();
+
+  const teamId = searchParams.get("teamId");
+
+  const { response: names, loading } = usePlayerNames(teamId);
+
+  if (loading) {
+    return null;
+  }
+
+  return (
+    <div className="container">
+      <SideBar title="Players" list={names} />
+    </div>
+  );
+}
