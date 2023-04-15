51b56adc02d34312878c4cb76791349ad1d7fe74
Son Do <dominhson580@gmail.com>
2023-04-15 08:30:53 +0700

    Navigate to main page if a resouces doesn't exist

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -1,4 +1,4 @@
-import { Link, useParams } from "react-router-dom";
+import { Link, Navigate, useParams } from "react-router-dom";
 import useTeam from "../hooks/useTeam";
 import Loading from "./Loading";
 import TeamLogo from "./TeamLogo";
@@ -8,37 +8,36 @@ export default function Team() {
 
   const { loading, response: team } = useTeam(teamId);
 
+  let body;
   if (loading) {
-    return null;
+    body = <Loading />;
+  } else if (team === null) {
+    body = <Navigate to="/teams" />;
+  } else {
+    body = (
+      <div style={{ width: "100%" }}>
+        <TeamLogo id={team.id} className="center" />
+
+        <h1 className="medium-header">{team.name}</h1>
+
+        <ul className="info-list row">
+          <li>
+            Est.<div>{team.established}</div>
+          </li>
+          <li>
+            Manager<div>{team.manager}</div>
+          </li>
+          <li>
+            Coach<div>{team.coach}</div>
+          </li>
+        </ul>
+
+        <Link className="center btn-main" to={`/${teamId}`}>
+          {team.name} Team Page
+        </Link>
+      </div>
+    );
   }
 
-  return (
-    <div className="panel">
-      {loading ? (
-        <Loading />
-      ) : (
-        <div style={{ width: "100%" }}>
-          <TeamLogo id={team.id} className="center" />
-
-          <h1 className="medium-header">{team.name}</h1>
-
-          <ul className="info-list row">
-            <li>
-              Est.<div>{team.established}</div>
-            </li>
-            <li>
-              Manager<div>{team.manager}</div>
-            </li>
-            <li>
-              Coach<div>{team.coach}</div>
-            </li>
-          </ul>
-
-          <Link className="center btn-main" to={`/${teamId}`}>
-            {team.name} Team Page
-          </Link>
-        </div>
-      )}
-    </div>
-  );
+  return <div className="panel">{body}</div>;
 }
