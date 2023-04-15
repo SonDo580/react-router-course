055e1309f8e5700be96d7fbe33d07a14c30c9eb2
Son Do <dominhson580@gmail.com>
2023-04-15 09:08:00 +0700

    Implement code splitting

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,17 @@
+import React, { Suspense } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Article from "./Article";
-import Articles from "./Articles";
-import Home from "./Home";
+
 import NavBar from "./NavBar";
-import Player from "./Player";
-import Players from "./Players";
-import Team from "./Team";
-import TeamPage from "./TeamPage";
-import Teams from "./Teams";
+import Loading from "./Loading";
+
+const Article = React.lazy(() => import("./Article"));
+const Articles = React.lazy(() => import("./Articles"));
+const Home = React.lazy(() => import("./Home"));
+const Player = React.lazy(() => import("./Player"));
+const Players = React.lazy(() => import("./Players"));
+const Team = React.lazy(() => import("./Team"));
+const TeamPage = React.lazy(() => import("./TeamPage"));
+const Teams = React.lazy(() => import("./Teams"));
 
 export default function App() {
   return (
@@ -15,39 +19,43 @@ export default function App() {
       <div>
         <NavBar />
 
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/players" element={<Players />}>
-            <Route path=":playerId" element={<Player />} />
-            <Route
-              path=""
-              element={
-                <div className="sidebar-instruction">Select a player</div>
-              }
-            />
-          </Route>
+        <Suspense fallback={<Loading />}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/players" element={<Players />}>
+              <Route path=":playerId" element={<Player />} />
+              <Route
+                path=""
+                element={
+                  <div className="sidebar-instruction">Select a player</div>
+                }
+              />
+            </Route>
 
-          <Route path="/teams" element={<Teams />}>
-            <Route path=":teamId" element={<Team />} />
-            <Route
-              path=""
-              element={<div className="sidebar-instruction">Select a team</div>}
-            />
-          </Route>
+            <Route path="/teams" element={<Teams />}>
+              <Route path=":teamId" element={<Team />} />
+              <Route
+                path=""
+                element={
+                  <div className="sidebar-instruction">Select a team</div>
+                }
+              />
+            </Route>
 
-          <Route path="/:teamId" element={<TeamPage />} />
+            <Route path="/:teamId" element={<TeamPage />} />
 
-          {/* No nested route here because TeamPage isn't in charge of rendering Articles */}
-          <Route path="/:teamId/articles" element={<Articles />}>
-            <Route path=":articleId" element={<Article />} />
-            <Route
-              path=""
-              element={
-                <div className="sidebar-instruction">Select an article</div>
-              }
-            />
-          </Route>
-        </Routes>
+            {/* No nested route here because TeamPage isn't in charge of rendering Articles */}
+            <Route path="/:teamId/articles" element={<Articles />}>
+              <Route path=":articleId" element={<Article />} />
+              <Route
+                path=""
+                element={
+                  <div className="sidebar-instruction">Select an article</div>
+                }
+              />
+            </Route>
+          </Routes>
+        </Suspense>
       </div>
     </BrowserRouter>
   );
